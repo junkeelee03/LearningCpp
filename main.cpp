5d106421c1114a11cdad4833e74160a276dca243
junkeelee03 <junlee01984@gmail.com>
2024-12-19 13:49:24 +0000

    I got the code to run: "g++ file_name" in the console terminal. However, I want the cout to work, but it doesn't output anything on the console. I'll commit first.

--- main.cpp
+++ main.cpp
@@ -3,27 +3,20 @@
 int main() {
     int file_size = 0; 
     double sales = 99.99; 
-    std:: cout << "Hello World!"; 
-    std:: cout << file_size; 
-    return 0;
-}
+    std:: cout << "Hello World!" << std:: endl; 
+    std:: cout << file_size << std:: endl; 
 
+    // Problem 1: Swap the value of two variables!
 
-// Problem 1: Swap the value of two variables!
-
-int swap() { 
     int a = 1; 
     int b = 2; 
     // Swap the variables! 
-    int c = a;
-    int d = b; 
+    int temporary = a;
+    a = b; 
+    b = temporary;
 
-    int a = d; 
-    int b = c; 
+    std:: cout << a << std:: endl; 
+    std:: cout << b << std:: endl;
 
-    cout << a; 
-    cout << b;
-
-    // Easier solution: only use one more variable. c = a, a = b, b = c. 
+    return 0;
 }
-main()
