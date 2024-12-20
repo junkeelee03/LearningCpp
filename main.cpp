e31f921bb4392097590538b99d34f4d8658f9fe8
junkeelee03 <junlee01984@gmail.com>
2024-12-20 14:15:19 +0000

    done with a video about the introduction to c++ , by Programming with Mosh. Where my line sits (where i'm typing, and there's supposed to be like an l shape to indicate where I'm typing) has converted to a black box and I don't know how to revert it. It's stressing me out, since it works differently.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime> 
+
+// Good for math: #include <cmath>
 
 using namespace std; // allows you to not type in "std::" before any of the commands that are from standard 
 
@@ -17,6 +21,7 @@ int main() {
 
     std:: cout << a << std:: endl; 
     std:: cout << b << std:: endl;
+
 
     // Using constants: The value of a variable does not change! 
     const double pi = 3.14;
@@ -55,6 +60,54 @@ int main() {
     double tempFahrenheit; 
     double tempCelsius = (tempFahrenheit - 32) * (5/9); 
     std:: cout << "The temperature in degrees Celsius: " << tempCelsius; 
+
+
+    /*
+    Multi- line comments! 
+    */
+
+
+    /* FUNDAMENTAL TYPES        TYPE        BYTES
+
+                                INT         4
+                                Short       2
+                                Long        4
+                                Long long   8
+
+                                FLOAT       4
+                                DOUBLE      8
+                                Long double 8
+
+                                BOOL        1
+                                CHAR        1
+    */ 
+
+    // CAN USE auto to infer the type! 
+    auto price = 99.99f; // FLOAT! The 'f' at the end of the number indicates a float instead of a double
+    auto expensive = 99999.99; // DOUBLE! automatically assumed it's a double, when a decimal is present
+    auto distance = 10000l; // LONG! The 'l' at the end of the number indicates a long instead of an int
+
+    // initializing a variable with brackets
+    int newnumber {}; // benefits: initializes to 0, doesn't run the code when the type definition and value don't match. 
+
+    // numbers in different sistems
+    int twofiftyfive = 255; // decimal 
+    twofiftyfive = 0b11111111; // binary 
+    twofiftyfive = 0xff; // hexadecimal
+
+    // CAREFUL when converting short -> integer, or any of the fundamental types! Can cause a loss in values due to varying memory sizes
+
+    // Random numbers: need to include cstdlib and ctime at the top of our code
+    long elapsedSeconds = time(nullpointer); 
+    srand(elapsedSeconds); // can shorten code by inputting time(nullpointer) directly
+    int number = rand() % 10; 
+    std:: cout << number; 
+
+    // Rolling a dice 
+    const short minValue = 1;
+    const short maxValue = 6;
+    (rand() % (maxValue - minValue + 1) ) + minValue // Random number formula: (random (maxvalue - minvalue + 1)) +minvalue
+     
 
     return 0;
 }
